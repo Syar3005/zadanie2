dd45ba8cc299060fbc7ead18d72f1f0a3936df5b
Syar3005 <113889600+Syar3005@users.noreply.github.com>
2022-10-04 21:38:50 +0300

    Add files via upload

---
+++ Lidjiev/Lidjiev/Program.cs
@@ -0,0 +1,24 @@
+﻿ using System;
+
+    namespace Lidjiev
+    { 
+      class MainClass
+      {
+        public static void Main()
+        {
+            int num654, num6, num54, num4, num5;
+            num654 = 654;
+            num6 = num654 / 100;
+            num54 = num654 % 100;
+            num5 = num54 / 10;
+            num4 = num54 % 10;
+            Console.WriteLine("{0}{1}{2}", num4, num5, num6);
+           
+        }
+      }
+    }
+
+
+
+
+ 
